6bd95a7e7389a31dc4acd4d338e1511de69da0d7
SysFaz <121228776+SysFaz@users.noreply.github.com>
2023-04-05 19:19:21 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,27 @@
-#!/bin/bash
+#! /bin/bash
 
 #backups
 cd ~
 mkdir backups
-cp ~/.zshrc ~/backups/zshrc.bac
+cp ~/.zshrc ~/backups/.zshrc.bac
 
 #setup git
 cd ~
 git clone https://github.com/SysFaz/kali-setup.git
 cd ~/kali-setup/scripts
 
+#vim setup
+rm ~/.vimrc
+rm ~/.vimrc.plug
+rm -r ~/.vim
+
+cd ~/kali-setup/configs/vim
+cp .vimrc ~/.vimrc
+cp .vimrc.plug ~/.vimrc.plug
+mkdir -p ~/.vim/plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#telebit setup
 chmod 700 telebit-setup.sh
 echo -e "\n#alias" >> ~/.zshrc
 echo "alias telebit='~/telebit'" >> ~/.zshrc
